ec6b60756ea7c7e25b375b23d3a25fc4fbd75f10
lujain998 <lujainalazzam98@gmail.com>
2024-09-23 15:40:28 +0300

    Task 1

---
+++ Task 1/dist/index.js
@@ -0,0 +1,25 @@
+"use strict";
+let num = 5;
+if (num % 2 === 0) {
+    console.log("Even");
+}
+else {
+    console.log("Odd");
+}
+let products = [
+    { name: "iPhone", price: 999 },
+    { name: "MacBook", price: 1299 },
+    { name: "iPad", price: 799 },
+    { name: "AirPods", price: 199 },
+    { name: "Headphones", price: 299 },
+];
+let expensiveProducts = products.filter(product => product.price > 1000);
+console.log(expensiveProducts);
+let users = [
+    { name: "John", age: 25, isAdmin: true },
+    { name: "Jane", age: 30, isAdmin: false },
+    { name: "Bob", age: 35, isAdmin: true },
+    { name: "Alice", age: 40, isAdmin: false },
+];
+let oldestAdmin = users.sort((a, b) => a.age - b.age)[0];
+console.log(oldestAdmin.name);
